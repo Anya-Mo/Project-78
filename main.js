7c3fccd5455419a26a76ded50c35810473ea99b3
Anya-Mo <72193720+Anya-Mo@users.noreply.github.com>
2021-03-06 12:24:54 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var image_array=["Family Book.jpg","Family Book Project.png","Family Book Project (1).jpg","Girl.jpg"];
+var name_array=["Family Book","Ayaz","Mitul","Anya"];
+var i=0;
+function Next() {
+    document.getElementById("image").src= image_array[i];
+    document.getElementById("family_member_name").innerHTML= name_array[i];
+    i++;
+    if (i == 4) {
+        i=0;
+    }
+}
